ad0c60cc71e849004147db2e0789bddb6d49e06a
Marina Lopez <marinalopezka@gmail.com>
2025-10-26 00:07:56 -0300

    create archivo script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const boton = document.querySelector("#boton-importante")
+
+  boton.addEventListener("click", function responseClick(){
+    alert("Gracias por tu interés.Pronto nos pondremos en contacto contigo")
+  })
