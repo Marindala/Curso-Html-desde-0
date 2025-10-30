388d85b6981c544318cc9fc0a81f654e8958fb6b
Marina Lopez <marinalopezka@gmail.com>
2025-10-29 22:09:05 -0300

    aplicado boton

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const boton = document.querySelector("#boton-importante")
 
   boton.addEventListener("click", function responseClick(){
-    alert("Gracias por tu interés.Pronto nos pondremos en contacto contigo")
+    boton.textContent = '¡Aplicado!'
+    
   })
