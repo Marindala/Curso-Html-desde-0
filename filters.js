2bab63c376916db88129cd9d9cf2b3fe67e38a68
Marina Lopez <marinalopezka@gmail.com>
2025-11-24 22:02:09 -0300

    empleos and filters

--- filters.js
+++ filters.js
@@ -3,8 +3,10 @@ const mensaje = document.querySelector('#filter-selected-value')
 
 filter.addEventListener('change', function () {
   const jobs = document.querySelectorAll('.job-listing-card')
+  console.log (jobs)
 
   const selectedValue = filter.value
+  console.log(selectedValue)
 
   if (selectedValue) {
     mensaje.textContent = `Has seleccionado: ${selectedValue}`
